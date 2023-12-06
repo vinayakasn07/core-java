1f284f61fefaf48e3450ae3a68a92c51db54b429
vinayakasn07 <152258603+vinayakasn07@users.noreply.github.com>
2023-12-06 11:39:25 +0530

    Add files via upload

---
+++ ThrowObject.java
@@ -0,0 +1,49 @@
+class ThrowObject
+{
+public static void main(String[]args)
+{
+Running();
+Yoga();
+Meditation();
+Pushup();
+Prayer();
+Cycling();
+Skipping();
+System.out.println("JAVA DEVELOPER");
+}
+
+static void Running()
+{
+System.out.println("good for health");
+}
+
+static void Yoga()
+{
+System.out.println("Mental health and physical health");
+}
+
+static void Meditation()
+{
+System.out.println("Mental health");
+}
+
+static void Pushup()
+{
+System.out.println("Body fitness");
+}
+
+static void Prayer()
+{
+System.out.println("Positive energy");
+}
+
+static void Cycling()
+{
+System.out.println("Muscular strength");
+}
+
+static void Skipping()
+{
+System.out.println("Increse height");
+}
+}
