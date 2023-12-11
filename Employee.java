b691642b81df8c8c529343c0872d18bd1cf5af7c
vinayakasn07 <vinayaka.xworkz@gmail.com>
2023-12-11 16:46:49 +0530

    Passing parameters to method

---
+++ Employee.java
@@ -0,0 +1,65 @@
+public class Employee
+{
+public static void main(String[]args)
+{
+
+empLockerNumber((byte)120);
+empId((short)30871);
+empSalary(100000);
+empMobileNumber(123456789);
+empPfDeductions(15000.00f);
+empEsiDeductions(13000.00d);
+firstAlphabetInEmpName('N');
+empPeformanceExcellent(true);
+
+}
+static byte empLockerNumber(byte a)
+{
+System.out.println(a);
+return a;
+}
+
+static short empId(short b)
+{
+System.out.println(b);
+return b;
+}
+
+static int empSalary(int c)
+{
+System.out.println(c);
+return c;
+}
+
+static long empMobileNumber(long d)
+{
+System.out.println(d);
+return d;
+}
+
+static float empPfDeductions(float e)
+{
+System.out.println(e);
+return e;
+}
+
+static double empEsiDeductions(double f)
+{
+System.out.println(f);
+return f;
+}
+
+static char firstAlphabetInEmpName(char g)
+{
+System.out.println(g);
+return g;
+}
+
+static boolean empPeformanceExcellent(boolean h)
+{
+System.out.println(h);
+return h;
+}
+
+
+}
