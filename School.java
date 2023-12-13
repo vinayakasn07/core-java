bad56c307d4a2c9c13ab29593e383ce4f95d67a1
vinayakasn07 <vinayaka.xworkz@gmail.com>
2023-12-13 15:03:43 +0530

    Example for Arrays

---
+++ School.java
@@ -0,0 +1,26 @@
+public class School
+{
+public static void main(String[]args)
+{
+byte[] v={(byte)1,(byte)2,(byte)3,(byte)4,(byte)5,(byte)6,(byte)7,(byte)8,(byte)9,(byte)10,(byte)11,(byte)12,(byte)13,(byte)14,(byte)15,(byte)16,(byte)17,(byte)18,(byte)19,(byte)20};
+short[] i={(short)11,(short)12,(short)13,(short)14,(short)156,(short)148,(short)1257,(short)145,(short)19,(short)26,(short)45,(short)689,(short)99,(short)88,(short)999,(short)45,(short)77,(short)98,(short)977,(short)200};
+int[] n={111,222,333,444,555,666,777,888,999,1111,2222,3333,4444,5555,6666,7777,8888,9999,11111,22222};
+long[] a={123456789L,236459781L,456123789L,789456123L,852963741L,456978123L,645987123L,236541789L,236541789L,564897123L,235647891L,2365478912L,456987236541L,2356541365L,56123322L,236541789L,567894123L,123654789L,236541897L,564789123L};
+float[] y={1.2f,1.3f,1.4f,1.5f,1.6f,1.7f,1.8f,1.9f,2.1f,2.2f,2.3f,2.4f,2.5f,2.6f,2.7f,2.8f,2.9f,3.0f,3.1f,3.2f,};
+double[] b={1.234d,1.5678d,2.13654d,2.78965d,3.12654d,4.5623178d,9.56487d,8.45623d,2.456898d,8.97546123d,9.5687412d,7.5684d,5.64897d,5.69874d,7.4569822d,5.64897d,5.64789d,6.45798d,7.564892d,6.5987412d};
+char[] k={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'};
+boolean[] z={true,false,true,true,false,true,false,true,false,false,true,true,false,false,true,false,true,false,true,false,true};
+
+for(int s=0; s<20; s++)
+{
+System.out.println(v[s]);
+System.out.println(i[s]);
+System.out.println(a[s]);
+System.out.println(a[s]);
+System.out.println(y[s]);
+System.out.println(a[s]);
+System.out.println(k[s]);
+System.out.println(a[s]);
+}
+}
+}
